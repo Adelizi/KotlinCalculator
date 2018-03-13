9e57baa18e31dcec15d92603b873009d6fd39f81
seabassR5 <seabassreyes@gmail.com>
2018-03-12 20:37:59 -0700

    Addition of Seabass Files

---
+++ src/SaveStrategy.kt
@@ -0,0 +1,18 @@
+import java.io.File;
+
+interface SaveStrategy {
+    fun save(filename: String, data: String)
+}
+class SaveAsText : SaveStrategy {
+    override fun save(filename: String, data: String){
+
+        val outString = data
+        File("$filename.txt").printWriter().use { out ->out.println(outString) }
+    }
+}
+class SaveAsXML : SaveStrategy {
+    override fun save(filename: String, data: String){
+        val outString = "<log>${data}</log>"
+        File("$filename.xml").printWriter().use { out ->out.println(outString) }
+    }
+}
